30c4205df61b34a12ada4ea52dd41ee4081ad6b1
EduardoFrugeri <Eduardodiasfrugeri@gmail.com>
2024-12-06 14:20:45 -0300

    js

---
+++ aula013/ambiente.js
@@ -0,0 +1,15 @@
+
+var c = 1
+while (c <= 2000000){
+    console.log(`Passo ${c}`)
+    c++ 
+}
+
+
+/*
+console.log('tudo certo')
+console.log('tudo certo')
+console.log('tudo certo')
+console.log('tudo certo')
+console.log('tudo certo')
+console.log('tudo certo') */
