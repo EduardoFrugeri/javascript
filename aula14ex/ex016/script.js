1ac724bb1f6a6c8d20be1b5d4d82a4c041ae8f1f
EduardoFrugeri <Eduardodiasfrugeri@gmail.com>
2024-12-08 19:02:49 -0300

    exercícios

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,21 @@
+function contar(){
+    let inicio = document.getElementById('txtInicio')
+    let fim = document.getElementById('txtFim')
+    let passo = document.getElementById('txtPassos')
+    let res = document.getElementById('res')
+
+
+    if (inicio.value.length == 0 || passo.value.length == 0 || fim.value.length == 0) {
+        window.alert('[ERRO] falta dados')} 
+        else{
+            res.innerHTML = 'Contando:'
+            let i = Number(inicio.value)
+            let f = Number(fim.value)
+            let p = Number(passo.value)
+
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += c
+            }
+        }
+        
+}
