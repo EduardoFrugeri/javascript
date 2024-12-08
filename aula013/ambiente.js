f32ba34b91627a3474bcf78595b749f0f20ddd39
EduardoFrugeri <Eduardodiasfrugeri@gmail.com>
2024-12-08 12:23:00 -0300

    repetições

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -1,15 +1,22 @@
-
+/*
 var c = 1
-while (c <= 2000000){
+while (c <= 10){
     console.log(`Passo ${c}`)
     c++ 
-}
+} */
+
+/*
+var c = 0
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 10) */
 
 
 /*
-console.log('tudo certo')
-console.log('tudo certo')
-console.log('tudo certo')
-console.log('tudo certo')
-console.log('tudo certo')
-console.log('tudo certo') */
+console.log('passo 1')
+console.log('passo 2')
+console.log('passo 3')
+console.log('passo 4')
+console.log('passo 5')
+console.log('passo 6') */
---
+++ aula014/ambiente2.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+while( c <= 5){
+    console.log(c)
+    c += 1
+}
+*/
+
+//------------------------
+console.log('começou')
+for(var c = 1; c <= 5; c+=1){
+    console.log(c)
+}
+console.log('terminou')
