325584b2e87133dbc20b2f2c4b978ddc970298ac
EduardoFrugeri <Eduardodiasfrugeri@gmail.com>
2024-12-10 21:53:39 -0300

    ex

---
+++ aula016ex/ex018/script.js
@@ -0,0 +1,28 @@
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
+
+function isNum(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, L){
+    if (L.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar(){
+    if(isNum(num.value) && ! inLista(num.value, valores)){
+        window.alert('Tudo ok')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+}
